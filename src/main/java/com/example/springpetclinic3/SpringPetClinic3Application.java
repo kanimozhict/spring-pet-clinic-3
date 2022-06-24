3cb1de1f12732b45a8f69525c07cde5562c1272d
kanimozhict <kanimozhict1571995@gmail.com>
2022-06-23 20:40:48 -0400

    initiall add of the project

---
+++ src/main/java/com/example/springpetclinic3/SpringPetClinic3Application.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinic3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinic3Application.class, args);
+    }
+
+}
